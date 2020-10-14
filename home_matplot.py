848f4e6af02fc0e4f01838aaccb40242ea9889d3
chiahn <a222056666@gmail.com>
2020-10-14 14:35:53 +0800

    lab

--- home_matplot.py
+++ home_matplot.py
@@ -77,81 +77,96 @@ class MatPlotMainWindow(QMainWindow, Ui_Matplot):
         self.pushButton.clicked.connect(lambda:self.plot_(3)) #點擊搜尋會跑出plot_函式的圖
         self.verticalLayout.addWidget(self.canvas) #將畫布添加到verticalLayout
         self.pushButton_3.clicked.connect(lambda:self.backpage(self.pushButton_3)) #點擊"上一頁"會跳到backpage函式，回到上一頁
-        self.lineEdit.textChanged.connect(self.searchitem)
-        self.listView.clicked.connect(self.inputtext)
-        self.qlist = list()
-        self.pushButton.clicked.connect(self.searchdisappear)
+        self.lineEdit.textChanged.connect(self.searchitem) #當lineEdit(文字框)發生文字變化時觸發self.searchitem
+        self.listView.clicked.connect(self.inputtext) #當listView(選單)裡的物件被點擊時觸發self.inputtext
+        self.qlist = list() #儲存listview的值
+        self.pushButton.clicked.connect(self.searchdisappear) #當pushButton(搜尋)被點擊時觸發self.searchdisappear
         self.stock_number_name = pd.read_csv("./stock_number_name.csv",index_col="名稱",encoding='utf8') #讀取stock_number_name.csv
-        self.tabWidget.currentChanged['int'].connect(self.onChange)
+        self.tabWidget.currentChanged['int'].connect(self.onChange) #當tab被切換時觸發self.onChange
 
-
+    #此函式為繪畫Matplotlib，month_number為控制tab的月份
     def plot_(self,month_number):
 
-        high_values = list()
-        low_values = list()
+        high_values = list() #最高價
+        low_values = list() #最低價
         plt.clf()
-        global bitch
+        global count_stock
         global stock_four_years
         global stock_last_date
+
+        # 用lineEdit的文字來找出檔案
         if self.lineEdit.text():
             try:
-                fuck = str(self.stock_number_name.loc[self.lineEdit.text()].values).strip('[]')
-                dick = pd.read_excel("./stock_index/indexOf" + fuck + ".xls",index_col="Date",encoding='utf8') #讀取index_new.xls
-                bitch = pd.read_csv("./allstock/twse_day_imformation_" + fuck + ".csv",index_col="日期",encoding='utf8') #讀取day_imformation_2330.csv
+                search_stock = str(self.stock_number_name.loc[self.lineEdit.text()].values).strip('[]')
+                original_stock = pd.read_excel("./stock_index/indexOf" + search_stock + ".xls",index_col="Date",encoding='utf8') #讀取index_new.xls
+                count_stock = pd.read_csv("./allstock/twse_day_imformation_" + search_stock + ".csv",index_col="日期",encoding='utf8') #讀取day_imformation_2330.csv
             except:
-                dick = pd.read_excel("./stock_index/indexOf" + self.lineEdit.text() + ".xls",index_col="Date",encoding='utf8') #讀取index_new.xls
-                bitch = pd.read_csv("./allstock/twse_day_imformation_" + self.lineEdit.text() + ".csv",index_col="日期",encoding='utf8') #讀取day_imformation_2330.csv
-        
-        bitch.columns = ["2330_stock","deal-stock","deal-money","open","high","low","close","up&down","Deal"] #設置每個項目的標題
-        dick.columns = ["Kvalue","Dvalue","RSI","DIF","MACD"] #設置每個項目的標題
-        stock_date = str(bitch.index[-1]).split('/')
+                original_stock = pd.read_excel("./stock_index/indexOf" + self.lineEdit.text() + ".xls",index_col="Date",encoding='utf8') #讀取index_new.xls
+                count_stock = pd.read_csv("./allstock/twse_day_imformation_" + self.lineEdit.text() + ".csv",index_col="日期",encoding='utf8') #讀取day_imformation_2330.csv
+        count_stock.columns = ["2330_stock","deal-stock","deal-money","open","high","low","close","up&down","Deal"] #設置每個項目的標題
+        original_stock.columns = ["Kvalue","Dvalue","RSI","DIF","MACD"] #設置每個項目的標題
+        ##############################
+
+        #count_stock將時間做整理，根據使用者所點選的時間顯示
+        stock_date = str(count_stock.index[-1]).split('/')
         stock_last_date = date(int(stock_date[0])+1911,int(stock_date[1]),int(stock_date[2]))
         stock_four_years = stock_last_date + relativedelta(months=-int(month_number)) #最後一筆資料減四年
         split_stock_date = str(stock_last_date).split('-') #將得到的日期做切片
         split_four_stock = str(stock_four_years).split('-')
         join_stock_date = str(int(split_stock_date[0])-1911) + '/' + str(split_stock_date[1]) + '/' + str(split_stock_date[2]) #將日期組回index的樣式
         join_four_stock = str(int(split_four_stock[0])-1911) + '/' + str(split_four_stock[1]) + '/' + str(split_four_stock[2])
-        bitch_two = bitch[join_four_stock:]
-        ##以下為過濾資料   
+        count_stock_two = count_stock[join_four_stock:]
+        ##############################
+
+        #以下為過濾資料   
         count = 0
-        for bitch_high in bitch_two['high']:
-            if bitch_high == '--':
-                bitch_two['high'][count] = '0'
+        for count_stock_high in count_stock_two['high']:
+            if count_stock_high == '--':
+                count_stock_two['high'][count] = '0'
             count += 1
         count = 0
-        for bitch_low in bitch_two['low']:
-            if bitch_low == '--':
-                bitch_two['low'][count] = '0'
+        for count_stock_low in count_stock_two['low']:
+            if count_stock_low == '--':
+                count_stock_two['low'][count] = '0'
             count += 1
         count = 0
-        for bitch_open in bitch_two['open']:
-            if bitch_open == '--':
-                bitch_two['open'][count] = '0'
+        for count_stock_open in count_stock_two['open']:
+            if count_stock_open == '--':
+                count_stock_two['open'][count] = '0'
             count += 1
         count = 0
-        for bitch_close in bitch_two['close']:
-            if bitch_close == '--':
-                bitch_two['close'][count] = '0'
+        for count_stock_close in count_stock_two['close']:
+            if count_stock_close == '--':
+                count_stock_two['close'][count] = '0'
             count += 1
         #####################################
-        for index,row in bitch_two.iterrows():
+
+        #找出圖上最大值跟最小值
+        for index,row in count_stock_two.iterrows():
             high_values.append(int(float(row["high"])))
             low_values.append(int(float(row["low"])))
         high_value = max(high_values)
         low_value = min(low_values)
-        #dick取四年的值
-        split_date = list(dick.index)[-1].split('/')
+        ####################################
+
+        #original_stock將時間做整理，根據使用者所點選的時間顯示
+        split_date = list(original_stock.index)[-1].split('/')
         last_date = date(int(split_date[0])+1911,int(split_date[1]),int(split_date[2]))
         four_years = last_date + relativedelta(months=-int(month_number))
         split_last_date = str(last_date).split('-')
         split_four_years = str(four_years).split('-')
         join_last_date = str(int(split_last_date[0])-1911) + '/' + str(split_last_date[1]) + '/' + str(split_last_date[2])
         join_four_years = str(int(split_four_years[0])-1911) + '/' + str(split_four_years[1]) + '/' + str(split_four_years[2])
-        dick_two = dick[join_four_years:] #依照條件取出dataframe的要求
-        if max(dick_two["MACD"]) > max(dick_two["DIF"]):
-            high_macd_value = max(dick_two["MACD"])
+        original_stock_two = original_stock[join_four_years:] #依照條件取出dataframe的要求
+        ##################################################
+
+        #找出MACD與DIF最大值
+        if max(original_stock_two["MACD"]) > max(original_stock_two["DIF"]):
+            high_macd_value = max(original_stock_two["MACD"])
         else:
-            high_macd_value = max(dick_two["DIF"])
+            high_macd_value = max(original_stock_two["DIF"])
+        #####################################
+
         #以下為四張圖的設定
         ax = self.figure.add_axes([0.05,0.65,0.9,0.30])
         ax2 = self.figure.add_axes([0.05,0.45,0.9,0.15], sharex=ax)
@@ -161,16 +176,18 @@ class MatPlotMainWindow(QMainWindow, Ui_Matplot):
         plt.setp(ax.get_xticklabels(), visible=False)
         plt.setp(ax2.get_xticklabels(), visible=False)
         plt.setp(ax3.get_xticklabels(), visible=False)
+        ###############################
 
-        mpf.candlestick2_ochl(ax, bitch_two['open'], bitch_two['close'], bitch_two['high'], bitch_two['low'], width=0.6, colorup='r', colordown='g', alpha=0.75); #生成蠟燭圖
+        mpf.candlestick2_ochl(ax, count_stock_two['open'], count_stock_two['close'], count_stock_two['high'], count_stock_two['low'], width=0.6, colorup='r', colordown='g', alpha=0.75); #生成蠟燭圖
 
+        #繪出十字線與設置文本等...
         def line(form,Ax,color,formname):
             len_y = len(form) #參數長度
             x = range(len_y) #從0~len_y
             _y = [list(form)[0]]*len_y #產生一個(len_y)長度的陣列
             line_x = Ax.plot(x, _y, color=color)[0] #畫水平線
             line_y = Ax.axvline(x=len_y-1, color='black',linestyle='--') #畫垂直線
-            ###以下為數值放置文本的設定
+            #以下為文本位置設定
             if high_value > 200:
                 high_200 = ((high_value/0.9)-(low_value*0.9)) / 30
                 target = int((low_value*0.9)+(30*int(high_200)))+0.5
@@ -205,6 +222,8 @@ class MatPlotMainWindow(QMainWindow, Ui_Matplot):
                 ax3_text = ax3.text(5,macd_target," ",fontsize=12)
             ax4_text = ax4.text(5,101," ",fontsize=12)
             ##############################################
+
+            #設置文本內容
             def motion(event):
                 try:
                     temp = form[int(np.round(event.xdata))] #根據對應的x值把y值提出來
@@ -221,63 +240,75 @@ class MatPlotMainWindow(QMainWindow, Ui_Matplot):
                         ax_text.set_position([55,target])
                         ax_text.set_text("low line:"+str(temp_as))
                         ax_text.set_color("lightgreen")
-                    if formname == 'dick_two["Kvalue"]':
+                    if formname == 'original_stock_two["Kvalue"]':
                         ax2_text.set_text("Kvalue:"+str(temp_as))
                         ax2_text.set_color("blue")
-                    if formname == 'dick_two["Dvalue"]':
+                    if formname == 'original_stock_two["Dvalue"]':
                         ax2_text.set_position([20,100.5])
                         ax2_text.set_text("Dvalue:"+str(temp_as))
                         ax2_text.set_color("red")
-                    if formname == 'dick_two["RSI"]':
+                    if formname == 'original_stock_two["RSI"]':
                         ax4_text.set_text("RSI:"+str(temp_as))
                         ax4_text.set_color("blue")
-                    if formname == 'dick_two["DIF"]':
+                    if formname == 'original_stock_two["DIF"]':
                         ax3_text.set_text("DIF:"+str(temp_as))
                         ax3_text.set_color("skyblue")
                     for i in range(len_y):
                         _y[i] = temp #將圖上得到的y值放入陣列裡
                     line_x.set_ydata(_y)
                     line_y.set_xdata(event.xdata)
-                    #print("X：",event.xdata)
-                    ######
+                    #####################################
+
                     self.figure.canvas.draw_idle() # 繪製matplotlib
                 except:
                     pass
             self.figure.canvas.mpl_connect('motion_notify_event', motion)
+        ###############################################################
 
+        sma_10 = talib.SMA(count_stock_two['low'], 10) #製作MA10
+        sma_20 = talib.SMA(count_stock_two['low'], 20) #製作MA20
+        sma_60 = talib.SMA(count_stock_two['low'], 60) #製作MA60
+        #sma_120 = talib.SMA(count_stock_two['low'], 120) #製作MA120
+        #sma_240 = talib.SMA(count_stock_two['low'], 240) #製作MA240
+        H_line,M_line,L_line=talib.BBANDS(count_stock_two['close'], timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)#製作布林通道
 
-        sma_10 = talib.SMA(bitch_two['low'], 10)
-        sma_20 = talib.SMA(bitch_two['low'], 20)
-        sma_60 = talib.SMA(bitch_two['low'], 60)
-        #sma_120 = talib.SMA(bitch_two['low'], 120)
-        #sma_240 = talib.SMA(bitch_two['low'], 240)
-        H_line,M_line,L_line=talib.BBANDS(bitch_two['close'], timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
         line(H_line,ax,'purple','sma_10')
         line(M_line,ax,'crimson','sma_20')
         line(L_line,ax,'lightgreen','sma_60')
+        #line(sma_10,ax,'crimson','sma_120')
+        #line(sma_20,ax,'crimson','sma_120')
+        #line(sma_60,ax,'crimson','sma_120')
         #line(sma_120,ax,'crimson','sma_120')
         #line(sma_240,ax,'purple','sma_240')
-        line(dick_two["Kvalue"],ax2,'blue','dick_two["Kvalue"]')
-        line(dick_two["Dvalue"],ax2,'red','dick_two["Dvalue"]')
-        line(dick_two["RSI"],ax4,'blue','dick_two["RSI"]')
-        line(dick_two["DIF"],ax3,'skyblue','dick_two["DIF"]')
+        line(original_stock_two["Kvalue"],ax2,'blue','original_stock_two["Kvalue"]')
+        line(original_stock_two["Dvalue"],ax2,'red','original_stock_two["Dvalue"]')
+        line(original_stock_two["RSI"],ax4,'blue','original_stock_two["RSI"]')
+        line(original_stock_two["DIF"],ax3,'skyblue','original_stock_two["DIF"]')
 
-        ax.plot(H_line,label='MA_10',color='crimson')
-        ax.plot(M_line,label='MA_20',color='purple')
-        ax.plot(L_line,label='MA_60',color='lightgreen')
-        #ax.plot(sma_120,label='MA_120',color='crimson')
-        #ax.plot(sma_240,label='MA_240',color='purple')
-        ax2.plot(dick_two["Kvalue"],label='Kvalue',color='blue')
-        ax2.plot(dick_two["Dvalue"],label='Dvalue',color='red')
-        ax4.plot(dick_two["RSI"],label='RSI',color='blue')
-        ax3.plot(dick_two["DIF"],label='DIF',color='skyblue')
+        ax.plot(H_line,label='MA_10',color='crimson') #繪製壓力線
+        ax.plot(M_line,label='MA_20',color='purple') #繪製MA20
+        ax.plot(L_line,label='MA_60',color='lightgreen') #繪製支撐線
+        #ax.plot(sma_10,label='MA_120',color='crimson') #繪製MA10
+        #ax.plot(sma_20,label='MA_120',color='crimson') #繪製MA20
+        #ax.plot(sma_60,label='MA_120',color='crimson') #繪製MA60
+        #ax.plot(sma_120,label='MA_120',color='crimson') #繪製MA120
+        #ax.plot(sma_240,label='MA_240',color='purple') #繪製MA240
+        ax2.plot(original_stock_two["Kvalue"],label='Kvalue',color='blue') #繪製Kvalue
+        ax2.plot(original_stock_two["Dvalue"],label='Dvalue',color='red') #繪製Dvalue
+        ax4.plot(original_stock_two["RSI"],label='RSI',color='blue') #繪製RSI
+        ax3.plot(original_stock_two["DIF"],label='DIF',color='skyblue') #繪製DIF
 
-        ax3.set_xlim(xmin=0,xmax=len(dick_two["MACD"]))
-        for row in range(len(dick_two["MACD"])):
-            ax3.bar(row,dick_two["MACD"][row],color='r' if dick_two["MACD"][row] > 0 else 'g')
+        #繪製柱狀MACD
+        ax3.set_xlim(xmin=0,xmax=len(original_stock_two["MACD"]))
+        for row in range(len(original_stock_two["MACD"])):
+            ax3.bar(row,original_stock_two["MACD"][row],color='r' if original_stock_two["MACD"][row] > 0 else 'g')
+        ######################################################
+        
+        #繪製X軸的日期
+        ax.set_xticks(range(0, len(count_stock_two.index), int(len(count_stock_two.index)/8)))
+        ax.set_xticklabels(count_stock_two.index[::int(len(count_stock_two.index)/8)])
+        ######################################################
 
-        ax.set_xticks(range(0, len(bitch_two.index), int(len(bitch_two.index)/8)))
-        ax.set_xticklabels(bitch_two.index[::int(len(bitch_two.index)/8)])
         #以下為調整蠟燭圖，防止圖超出邊界
         if high_value > 200:
             ax.set_yticks(range(int(low_value*0.9),int(high_value/0.9),30))
@@ -289,26 +320,19 @@ class MatPlotMainWindow(QMainWindow, Ui_Matplot):
             ax.set_yticks(range(int(low_value-2),int(high_value+3),3))
         ###########################
 
-        self.canvas.draw()  
+        self.canvas.draw() #將圖給畫出來 
+
     # def backpage(self,btn3): #隱藏matplotlib，顯示主頁
     #     self.hide()
     #     HwWin.show()
 
-    # def deal_stock(self,bitch,view_month):
-    #     global bitch_two
-    #     self.stock_date = str(bitch.index[-1]).split('/')
-    #     self.stock_last_date = date(int(self.stock_date[0])+1911,int(self.stock_date[1]),int(self.stock_date[2]))
-    #     self.stock_four_years = self.stock_last_date + relativedelta(months=-int(view_month)) #最後一筆資料減四年
-    #     self.split_stock_date = str(self.stock_last_date).split('-') #將得到的日期做切片
-    #     self.split_four_stock = str(self.stock_four_years).split('-')
-    #     self.join_stock_date = str(int(self.split_stock_date[0])-1911) + '/' + str(self.split_stock_date[1]) + '/' + str(self.split_stock_date[2]) #將日期組回index的樣式
-    #     self.join_four_stock = str(int(self.split_four_stock[0])-1911) + '/' + str(self.split_four_stock[1]) + '/' + str(self.split_four_stock[2])
-    #     bitch_two = bitch[self.join_four_stock:] 
-
+    #關於搜尋的功能
     def searchitem(self,text):
-        self.qlist.clear()
+        self.qlist.clear() #防止讀到上一次搜尋的結果
+        #若text為空，則隱藏listView
         if (text == ''):
             self.listView.hide()
+        #否則將代號讀入qlist
         else:
             self.listView.show()
             for stock_number in self.stock_number_name['代號']:
@@ -323,26 +347,35 @@ class MatPlotMainWindow(QMainWindow, Ui_Matplot):
             slm = QStringListModel()
             slm.setStringList(self.qlist)
             self.listView.setModel(slm)
+    #########################################################
 
+    #當點選listView內容時，直接將內容填入lineEdit
     def inputtext(self,qModelIndex):
         self.lineEdit.setText(self.qlist[qModelIndex.row()])
         self.listView.hide()
+    ########################################################
 
+    #當點擊搜尋時，讓listView隱藏
     def searchdisappear(self):
         self.listView.hide()
+    #########################################################
 
+    #當tab被切換時，根據點選的月份顯示
     def onChange(self,index):
-        currentTab = self.tabWidget.currentIndex()
-        if currentTab == 0:
-            self.plot_(3)
-            self.verticalLayout.addWidget(self.canvas) #將畫布添加到verticalLayout
-        if currentTab == 1:
-            self.plot_(6)
-            self.verticalLayout_2.addWidget(self.canvas) #將畫布添加到verticalLayout_2
-        if currentTab == 2:
-            self.plot_(12)
-            self.verticalLayout_3.addWidget(self.canvas) #將畫布添加到verticalLayout_3
-            
+        try:
+            currentTab = self.tabWidget.currentIndex()
+            if currentTab == 0:
+                self.plot_(3)
+                self.verticalLayout.addWidget(self.canvas) #將畫布添加到verticalLayout
+            if currentTab == 1:
+                self.plot_(6)
+                self.verticalLayout_2.addWidget(self.canvas) #將畫布添加到verticalLayout_2
+            if currentTab == 2:
+                self.plot_(12)
+                self.verticalLayout_3.addWidget(self.canvas) #將畫布添加到verticalLayout_3
+        except:
+            pass
+    #########################################################
 
 if __name__=="__main__":  
     app = QApplication(sys.argv)  
